c47e2dc6126d0929899b55c5799ef5a46df2c5bb
Thyago Jesus <thyagojesus.a@gmail.com>
2022-06-20 22:23:51 -0300

    created GET and POST of tasks

--- src/index.js
+++ src/index.js
@@ -12,7 +12,14 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
     // Complete aqui
-    const { username } = request.header;
+    const { username } = request.headers;
+    const user = users.find((user) => user.username === username);
+    if (!user) {
+        return response.status(400).send({ error: "User not found" });
+    }
+    request.user = user;
+
+    return next();
 }
 
 app.post("/users", (request, response) => {
@@ -53,10 +60,26 @@ app.get("/users", (request, response) => {
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
     // Complete aqui
+    const { user } = request;
+
+    return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
     // Complete aqui
+    const { user } = request;
+    const { title, deadline } = request.body;
+
+    const todo = {
+        id: uuidv4(),
+        title: title,
+        done: false,
+        deadline: new Date(deadline),
+        created_at: new Date(),
+    };
+    user.todos.push(todo);
+
+    return response.status(201).json({ message: "task created successfully" });
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
